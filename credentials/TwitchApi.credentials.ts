894c777bda07a5ebede801834980e59979e8d82b
milanjrodd <milanrodd@gmail.com>
2023-11-23 10:41:49 +0300

    change credential password type

--- credentials/TwitchApi.credentials.ts
+++ credentials/TwitchApi.credentials.ts
@@ -1,23 +1,27 @@
 import {
 	ICredentialType,
+	INodeProperties,
 	NodePropertyTypes,
-} from 'n8n-workflow';
+} from "n8n-workflow";
 
 export class TwitchApi implements ICredentialType {
-	name = 'twitchApi';
-	displayName = 'Twitch API';
-	properties = [
+	name = "twitchApi";
+	displayName = "Twitch API";
+	properties: INodeProperties[] = [
 		{
-			displayName: 'Client ID',
-			name: 'clientId',
-			type: 'string' as NodePropertyTypes,
-			default: '',
+			displayName: "Client ID",
+			name: "clientId",
+			type: "string" as NodePropertyTypes,
+			default: "",
 		},
 		{
-			displayName: 'Client Secret',
-			name: 'clientSecret',
-			type: 'string' as NodePropertyTypes,
-			default: '',
+			displayName: "Client Secret",
+			name: "clientSecret",
+			type: "string" as NodePropertyTypes,
+			default: "",
+			typeOptions: {
+				password: true,
+			},
 		},
 	];
 }
